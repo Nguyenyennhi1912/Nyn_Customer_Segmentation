b388c5a89f51b088df94d178e38617d08d804d6f
Nguyen Yen Nhi <109221023+Nguyenyennhi1912@users.noreply.github.com>
2024-04-03 08:46:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -103,6 +103,9 @@ def main():
                 - **Recency**: How much time has elapsed since a customer’s last activity or transaction with the brand? Activity is usually a purchase, although variations are sometimes used, e.g., the last visit to a website or use of a mobile app. In most cases, the more recently a customer has interacted or transacted with a brand, the more likely that customer will be responsive to communications from the brand. 
                 - **Frequency**: How often has a customer transacted or interacted with the brand during a particular period of time? Clearly, customers with frequent activities are more engaged, and probably more loyal, than customers who rarely do so. And one-time-only customers are in a class of their own. 
                 - **Monetary**: Also referred to as “monetary value,” this factor reflects how much a customer has spent with the brand during a particular period of time. Big spenders should usually be treated differently than customers who spend little. Looking at monetary divided by frequency indicates the average purchase amount – an important secondary factor to consider when segmenting customers. """)
+        
+        st.subheader ("📃 About Dataset")        
+        st.subheader("[Link data](https://csc.edu.vn)")
         st.subheader ("👭 Team members")
         st.write("""
         Le Yen Ha
