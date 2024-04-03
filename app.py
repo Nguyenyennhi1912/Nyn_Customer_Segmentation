118535674029d51ed4ce16e4a60a8c3258350a2d
Nguyen Yen Nhi <109221023+Nguyenyennhi1912@users.noreply.github.com>
2024-04-03 08:11:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def main():
         with col1:
             st.write(' ')
         with col2:
-            st.image("customer-segmentation.jpg", width=600)  
+            st.image("customer-segmentation.jpg", width=500)  
         with col3:
             st.write(' ')
           
